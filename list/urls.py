84eddd6ae22ea5e353fec6b9d626356337fabd1e
Jayant-786 <jayant.jain.min19@itbhu.ac.in>
2023-02-16 15:03:04 +0530

    first commit

--- list/urls.py
+++ list/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # path('', views.home , name ='home'),
     path('', TodoView.as_view()),
     path('<int:pk>/', TodoView.as_view()),
-    path('mark_as_done/<int:pk>/',MarkdoneAPIView.as_view()),
+    path('mark_as_done/<int:pk>/',MarkdoneAPIView.as_view()), # To mark the task as complete/incomplete
 
 ]
--- list/views.py
+++ list/views.py
@@ -7,42 +7,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-# from rest_framework import generics
-
-
-# class ListTask(generics.ListAPIView):
-#     queryset = Tasks.objects.all()
-#     serializer_class = TaskSerializer
-
-# class CreateTask(generics.CreateAPIView):
-#     queryset = Tasks.objects.all()
-#     serializer_class = TaskSerializer
-
-# class UpdateTask(generics.RetrieveUpdateAPIView):
-#     queryset = Tasks.objects.all()
-#     serializer_class = TaskSerializer
-
-# class DestroyTask(generics.DestroyAPIView):
-#     queryset = Tasks.objects.all()
-#     serializer_class = TaskSerializer
 
 class TodoView(APIView):
-    def object(self):
-        task = Tasks.objects.all()
-        serializer = TasksSerializer(task)
-        return Response(serializer.data)
-
-
+    
     def get_object(self, pk):
         try:
-            print(pk)
             return Tasks.objects.get(pk=pk)
         except Tasks.DoesNotExist:
             raise Http404
 
-    def get(self, request, pk=None, format=None):
-        serializer = ""
-        if pk is None:
+    def get(self, request, pk=None, format=None):           
+        if pk is None:                                          # 
             task = Tasks.objects.all()
             serializer = TasksSerializer(task,many=True)
         else:
@@ -51,12 +26,13 @@ class TodoView(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        # task = self.get_object(pk)
+       
         serializer = TasksSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def put(self, request, pk, format=None):
         task = self.get_object(pk)
         serializer = TasksSerializer(task, data=request.data)
@@ -69,6 +45,7 @@ class TodoView(APIView):
         task = self.get_object(pk)
         task.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+        
 
 class MarkdoneAPIView(APIView):
    
